beedc68b131ce82ec5b0dd9dae754493898af02b
cesar.vega <cesar.vega@tmceurope.com>
2026-02-14 12:26:55 +0100

    feat: fix gitignore, postcss config and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
-import "./globals.css"
+import type { Metadata } from 'next'
+import React from "react"
+import './globals.css'
+
+export const metadata: Metadata = {}
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
-  return children
+}>) {
+  return (
+    <html lang="es">
+      <body >  
+          {children}
+      </body>
+    </html>
+  )
 }
